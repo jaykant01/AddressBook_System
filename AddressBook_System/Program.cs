5b1a5b17a4286405f40d8aae2714edbc6d6c8740
jaykant01 <jaykanty24092004@gmail.com>
2026-01-27 11:41:28 +0530

    [Jaykant] Add .Address Book Dicitonary and refactor feature added

--- AddressBook_System/Program.cs
+++ AddressBook_System/Program.cs
@@ -4,59 +4,104 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Welcome to Address Book Program");
+
+        // Dictionary to store multiple Address Books
+        Dictionary<string, AddressBook> addressBookDictionary = new Dictionary<string, AddressBook>();
+
         char choice;
 
+        //Add Multiple Address Books
         do
         {
-            Console.WriteLine("Welcome to Address Book Program");
-            //Add new Person
-            Console.WriteLine("Enter First Name:");
-            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter Address Book Name:");
+            string addressBookName = Console.ReadLine();
 
-            Console.WriteLine("Enter Last Name:");
-            string lastName = Console.ReadLine();
+            if (!addressBookDictionary.ContainsKey(addressBookName))
+            {
+                AddressBook addressBook = new AddressBook();
+                addressBookDictionary.Add(addressBookName, addressBook);
 
-            Console.WriteLine("Enter Address:");
-            string address = Console.ReadLine();
+                Console.WriteLine("Address Book added successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Address Book with this name already exists.");
+            }
 
-            Console.WriteLine("Enter City:");
-            string city = Console.ReadLine();
-
-            Console.WriteLine("Enter State:");
-            string state = Console.ReadLine();
-
-            Console.WriteLine("Enter Zip:");
-            int zip = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter Phone Number:");
-            double phoneNumber = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Enter Email:");
-            string email = Console.ReadLine();
-
-            CreateContacts person = new CreateContacts(firstName, lastName, address, city, state, zip, phoneNumber, email);
-            AddressBook addressBook = new AddressBook();
-            addressBook.AddContact(person);
-            
-            Console.WriteLine("Contact added successfully.");
-            addressBook.DisplayContact();
-
-            Console.WriteLine("Do you want to add another contact? (y/n)");
+            Console.WriteLine("Do you want to add another Address Book? (y/n)");
             choice = Console.ReadLine()[0];
 
-            // Edit Contact
-            //Console.WriteLine("Enter First Name of the contact to edit:");
-            //string nameToEdit = Console.ReadLine();
+        } while (choice == 'y' || choice == 'Y');
 
-            //addressBook.EditContact(nameToEdit);
+        // Select Address book To Add Contacts
+        Console.WriteLine("Enter Address Book Name to add contacts:");
+        string selectedBookName = Console.ReadLine();
+        if (addressBookDictionary.ContainsKey(selectedBookName))
+        {
+            AddressBook addressBook = addressBookDictionary[selectedBookName];
 
-            // Delete Contact
+            //Add Multiple Contacts
+            do
+            {
+                Console.WriteLine("Enter First Name:");
+                string firstName = Console.ReadLine();
+
+                Console.WriteLine("Enter Last Name:");
+                string lastName = Console.ReadLine();
+
+                Console.WriteLine("Enter Address:");
+                string address = Console.ReadLine();
+
+                Console.WriteLine("Enter City:");
+                string city = Console.ReadLine();
+
+                Console.WriteLine("Enter State:");
+                string state = Console.ReadLine();
+
+                Console.WriteLine("Enter Zip:");
+                int zip = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter Phone Number:");
+                double phoneNumber = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter Email:");
+                string email = Console.ReadLine();
+
+                CreateContacts person = new CreateContacts(firstName, lastName, address, city, state, zip, phoneNumber, email);
+
+                addressBook.AddContact(person);
+
+                Console.WriteLine("Contact added successfully.");
+
+                Console.WriteLine("Do you want to add another contact? (y/n)");
+                choice = Console.ReadLine()[0];
+
+            } while (choice == 'y' || choice == 'Y');
+
+            //Display Contacts
+            addressBook.DisplayContact();
+
+            //Edit Contact
+            Console.WriteLine("Enter First Name of the contact to edit:");
+            string nameToEdit = Console.ReadLine();
+
+            addressBook.EditContact(nameToEdit);
+
+            //Delete Contact
             Console.WriteLine("Enter First Name of the contact to delete:");
             string nameToDelete = Console.ReadLine();
 
             addressBook.DeleteContact(nameToDelete);
-        } 
-        while (choice == 'y' || choice == 'Y');
+
+            // Final Display
+            Console.WriteLine("Final Contact List:");
+            addressBook.DisplayContact();
+        }
+        else
+        {
+            Console.WriteLine("Address Book not found.");
+        }
 
         Console.ReadKey();
     }
